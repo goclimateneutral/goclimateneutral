b3df8baf21365410f86f7d027dcfd2da0f376675
Emma Bäckström <backstromemma@gmail.com>
2020-05-28 13:59:16 +0200

    code climate

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,7 +19,11 @@ module Users
       end
 
       respond_to do |format|
-        format.html { render 'new_alternative' if alternative_signup? && !(cookies[:new_design].present? || params.key?('new_design')) }
+        format.html do
+          if alternative_signup? && !(cookies[:new_design].present? || params.key?('new_design'))
+            render 'new_alternative'
+          end
+        end
         format.json { render_price_json }
       end
     end
